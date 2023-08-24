a7d6867c232e3199983f9d03cd8103a36c8405ff
Abuemkeze Chu <abuemkeze@gmail.com>
2023-08-24 06:41:46 +0100

    feat: fibonacci series using for loop

--- fibonacci.js
+++ fibonacci.js
@@ -1,1 +1,20 @@
 'use strict'
+
+// Using loops
+
+const fibonacci = (num) => {
+  let num1 = 0
+  let num2 = 1
+  let sum
+  let arr = [num1,num2]
+  for(let i = 2; i < num; i++){
+    sum = num1 + num2
+    num1 = num2
+    num2 = sum
+    arr.push(sum)
+  }
+  return arr
+}
+
+const ans = fibonacci(8)
+console.log(ans);
